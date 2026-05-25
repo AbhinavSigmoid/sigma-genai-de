21cb87f93c75f547d5eaf71ec92c588b3f44d77b
AS-MAC-1133 <as-mac-1133@AS-MAC-1133s-MacBook-Air.local>
2026-05-25 15:18:00 +0530

    Abhinav day 6

--- day6/lab/sample_data.py
+++ day6/lab/sample_data.py
@@ -163,11 +163,11 @@ NL2SQL_QUESTIONS = [
 # ── SNOWFLAKE CONFIG TEMPLATE ──────────────────────────────
 
 SNOWFLAKE_CONFIG_TEMPLATE = {
-    "user": "student_genai",
-    "account": "GEJKIOG-TKC55632",
+    "user": "ABHINAVSIGMOID",
+    "account": "BMNQHFN-KQ20820",
     "private_key_path": "../bonus/student_key.p8",
     "database": "SIGMA_DE",
     "schema": "PUBLIC",
     "warehouse": "COMPUTE_WH",
-    "role": "STUDENT_CORTEX",
+   "role": "ACCOUNTADMIN",
 }
